a1812d4635b32c556c0e562df23fa39b5fe1c9a7
Till Gerken <till.gerken@gmail.com>
2024-10-01 08:09:19 +0200

    first commit

---
+++ example/talker_azure_application_insights_observer_example.dart
@@ -0,0 +1,6 @@
+import 'package:talker_azure_application_insights_observer/talker_azure_application_insights_observer.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/talker_azure_application_insights_observer.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/talker_azure_application_insights_observer_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
