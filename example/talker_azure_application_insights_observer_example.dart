3ee9ceda2f97e2b800ad8c108d5b8c1600301342
Till Gerken <till.gerken@gmail.com>
2024-10-01 09:26:41 +0200

    First implementation

--- example/talker_azure_application_insights_observer_example.dart
+++ example/talker_azure_application_insights_observer_example.dart
@@ -1,6 +1,11 @@
+import 'package:talker/talker.dart';
 import 'package:talker_azure_application_insights_observer/talker_azure_application_insights_observer.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  final azureInsightsObserver = TalkerAzureApplicationInsightsObserver(
+      connectionString: '<APPLICATION_INSIGHTS_CONNECTION_STRING>');
+
+  final talker = Talker(observer: azureInsightsObserver);
+
+  talker.info('Info message sent via Talker to Azure Application Insights');
 }
---
+++ lib/src/talker_azure_application_insights_observer.dart
@@ -0,0 +1,77 @@
+import 'package:azure_application_insights/azure_application_insights.dart';
+import 'package:http/http.dart';
+import 'package:talker/talker.dart';
+
+/// Talker observer that logs data to Azure Application Insights
+class TalkerAzureApplicationInsightsObserver extends TalkerObserver {
+  late BufferedProcessor _processor;
+  late TelemetryClient _telemetryClient;
+
+  TalkerAzureApplicationInsightsObserver(
+      {required String connectionString, Client? httpClient}) {
+    final connectionStringElements = connectionString.split(';');
+
+    final instrumentationKeyCandidates = connectionStringElements
+        .where((e) => e.startsWith('InstrumentationKey='))
+        .toList();
+
+    // if we get an incorrect connection string, don't log anything
+    if (instrumentationKeyCandidates.isEmpty) {
+      return;
+    }
+
+    final instrumentationKey = instrumentationKeyCandidates.first
+        .substring('InstrumentationKey='.length);
+
+    _processor = BufferedProcessor(
+      next: TransmissionProcessor(
+        instrumentationKey: instrumentationKey,
+        httpClient: httpClient ?? Client(),
+        timeout: const Duration(seconds: 10),
+      ),
+    );
+
+    _telemetryClient = TelemetryClient(processor: _processor);
+  }
+
+  Severity _talkerLevelToSeverity(LogLevel logLevel) {
+    switch (logLevel) {
+      case LogLevel.critical:
+        return Severity.critical;
+      case LogLevel.error:
+        return Severity.error;
+      case LogLevel.warning:
+        return Severity.warning;
+      case LogLevel.info:
+        return Severity.information;
+      case LogLevel.verbose:
+        return Severity.verbose;
+      case LogLevel.debug:
+        return Severity.verbose;
+    }
+  }
+
+  @override
+  void onError(TalkerError err) {
+    _telemetryClient.trackError(
+      severity: _talkerLevelToSeverity(err.logLevel ?? LogLevel.error),
+      error: err.generateTextMessage(),
+    );
+  }
+
+  @override
+  void onException(TalkerException err) {
+    _telemetryClient.trackError(
+      severity: _talkerLevelToSeverity(err.logLevel ?? LogLevel.error),
+      error: err.generateTextMessage(),
+    );
+  }
+
+  @override
+  void onLog(TalkerData log) {
+    _telemetryClient.trackTrace(
+      severity: _talkerLevelToSeverity(log.logLevel ?? LogLevel.info),
+      message: log.generateTextMessage(),
+    );
+  }
+}
